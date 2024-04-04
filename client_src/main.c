0f5489afe9321d559b7bd91c65edbaec5062f474
Petar Evtimov <petar.evtimov@yahoo.com>
2024-04-04 16:06:11 +0200

    comit

--- client_src/main.c
+++ client_src/main.c
@@ -1,4 +1,26 @@
-int main(void)
-{
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h> // for PIDe
+#include "../ft_printf/ft_printf.h"
+#include "../libft/libft.h"
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{	
+	pid_t	pid_srv;
+	char	*text;
+
+	if(argc == 3)
+	{
+		pid_srv = ft_atoi(argv[1]);
+		text = argv[2];
+	}
+
+	ft_printf("%d / %s", pid_srv, text);
+
+	ft_printf("USR1: %d",kill(pid_srv, SIGUSR1));
+	pause();
+	ft_printf("USR2: %d",kill(pid_srv, SIGUSR2));
+
 	return(0);
 }
--- server_src/main.c
+++ server_src/main.c
@@ -1,12 +1,43 @@
 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h> // for PIDe
+#include "../ft_printf/ft_printf.h"
+#include "../libft/libft.h"
+#include <signal.h>
 
+void handler(int signum)
+{
+	if (signum == SIGUSR1)
+	{
+		ft_printf("SIGUSR1\n");
+		
+	}
+	else if (signum == SIGUSR2)
+	{
+		ft_printf("SIGUSR2\n");
+	}
+	else
+		ft_printf("signum: %d\n", signum);
+}
 
 int main(void)
 {
+	pid_t	pid_srv;
+	pid_srv = getpid();
+
+	ft_printf("Server PID: %d\n", pid_srv);
 	
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+
+	while (1)
+	{
+		pause();
+	}
 	return(0);
 }
-
+ 
 
 //server
 	// print PID
